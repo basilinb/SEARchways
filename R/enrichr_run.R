fd09efbb8b94ab5632e90e1ae50c82682dc52a0a
Basilin Benson <basilinb@gmail.com>
2022-03-22 15:04:14 -0700

    Update

--- R/enrichr_run.R
+++ R/enrichr_run.R
@@ -1,3 +1,100 @@
+
+
+
+
+##' onLoad hook to setup package options
+##'
+##' onLoad hook to setup package options and to check connection to website
+##' @title onLoad hook to setup package options
+##' @param libname (Required). Library name
+##' @param pkgname (Required). Package name
+##' @return NULL
+##' @author Wajid Jawaid \email{wj241@alumni.cam.ac.uk}
+.onAttach <- function(libname, pkgname) {
+  options(enrichR.base.address = "https://maayanlab.cloud/Enrichr/")
+  options(enrichR.live = TRUE)
+  packageStartupMessage("Welcome to enrichR\nChecking connection ... ", appendLF = TRUE)
+  options(modEnrichR.use = TRUE)
+  options(enrichR.sites.base.address = "https://maayanlab.cloud/")
+  options(enrichR.sites = c("Enrichr", "FlyEnrichr", "WormEnrichr", "YeastEnrichr", "FishEnrichr", "OxEnrichr"))
+  if (getOption("modEnrichR.use")) {
+    listEnrichrSites()
+  } else {
+    getEnrichr(url=paste0(getOption("enrichR.base.address"), "datasetStatistics"))
+    packageStartupMessage("Enrichr ... ", appendLF = FALSE)
+    if (getOption("enrichR.live")) packageStartupMessage("Connection is Live!")
+  }
+}
+
+
+##' Helper function
+##'
+##' Helper function for GET
+##' @title Helper function for GET
+##' @param url (Required). URL address requested
+##' @param ... (Optional). Additional parameters to pass to GET
+##' @return same as GET
+##' @author Wajid Jawaid \email{wj241@alumni.cam.ac.uk}
+##' @author I-Hsuan Lin \email{i-hsuan.lin@manchester.ac.uk}
+##' @importFrom httr GET
+##' @importFrom httr status_code
+##' @importFrom httr http_status
+getEnrichr <- function(url, ...) {
+  options(enrichR.live = FALSE)
+  tryCatch({
+    x <- GET(url = url, ...)
+    code <- status_code(x)
+    if(code != 200) {
+      # Error with status code
+      message(http_status(code)$message)
+    } else {
+      # OK/success
+      options(enrichR.live = TRUE)
+      invisible(x)
+    }
+  },
+  # Warning message
+  warning = function(warn) {
+    message(warn); message("") # force newline
+  },
+  # Error without status code
+  error = function(err) {
+    message(err); message("") # force newline
+  },
+  finally = function() {
+    invisible(x)
+  })
+}
+
+##' List modEnrichr Websites
+##'
+##' List Enrichr Websites
+##' @title List Enrichr Websites
+##' @return print Enrichr Website status
+##' @author Alexander Blume
+##' @param ... (Optional  Additional parameters)
+##' @export
+listEnrichrSites <- function(...) {
+  for (site in getOption("enrichR.sites")) {
+    getEnrichr(url = paste0(getOption("enrichR.sites.base.address"), site, "/", "datasetStatistics"))
+    packageStartupMessage(paste0(site, " ... "), appendLF = FALSE)
+    if (paste0(getOption("enrichR.sites.base.address"), site, "/")  == getOption("enrichR.base.address")) {
+      if (getOption("enrichR.live")) packageStartupMessage("Connection is Live!")
+    } else
+      if (getOption("enrichR.live")) packageStartupMessage("Connection is available!")
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
 #' Run EnrichR function on multiple Geneset databases on either gene list or modules of intertest
 #'
 #' @param gene_mod_list Here input either Genelist in HGNC or Ensembl ID format or input module object output from module function
@@ -5,9 +102,7 @@
 #' @param gene_id gene id is either HGNC or Ensembl
 #' @param dbs Enter the geneset database you want to run Enrichr on the default is MSigDB Hallmark 2020 to check available database run enrichR::listEnrichrDb()
 #' @param mod_of_interest Input your modules of interest by default will run modules
-#' @importFrom enrichR enrichr
-#' @importFrom enrichR listEnrichrSites
-#' @importFrom enrichR listEnrichrDbs
+
 #' @return list with enrichr result for each geneset database
 #'
 #' @export
@@ -21,8 +116,6 @@ enrichr_run <- function(gene_mod_list, type = "gene",gene_id = "HGNC",dbs = c("M
   all_genes <- biomaRt::getBM(attributes=c('ensembl_gene_id','gene_biotype','hgnc_symbol'), mart = ensembl) %>%
     dplyr::filter(gene_biotype == "protein_coding")
   #Setting the website for Enrichr to run
-  #enrichR::listEnrichrSites()
-  #enrichR::listEnrichrDbs()
   enrichR::setEnrichrSite("Enrichr")
   #Defining two empty lists
   enrich_list <- list()
